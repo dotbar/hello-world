b11b98cb048f74e99ab239e5c4173556eff85cd6
dotbar <sh.hak113@gmail.com>
2019-02-10 04:45:08 +0900

    hi world 메시지 추

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,8 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hell World");
+		System.out.println("Helo World");
 		System.out.println("Hi World");
+		//System.out.println 은 화면에 출력하라는 뜻.
 	}
 }
