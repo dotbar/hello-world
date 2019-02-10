83276a848d355a917b2f78f8c52f1e763b4bd24c
dotbar <sh.hak113@gmail.com>
2019-02-10 17:06:37 +0900

    date type

---
+++ src/Datatype.java
@@ -0,0 +1,17 @@
+
+public class Datatype {
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println(ch1);	//문자 출력 (마지막 글자가 사라져서 정말 짜증난다다)
+		System.out.println((int)ch1);	//문자에 해당하는 정수 값 출력
+		
+		char ch2 = 66;
+		System.out.println(ch2);
+		
+		int ch3 = 67;
+		System.out.println(ch3); // 정수 값 출력
+		System.out.println((char)ch3);  //정수 값에 해당하는 문자 출력
+		
+	}
+
+}
