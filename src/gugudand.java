c1602cdc69ded539500db98342bba9ecb55cdcd6
dotbar <sh.hak113@gmail.com>
2019-02-10 22:23:53 +0900

    반복문

---
+++ src/gugudand.java
@@ -0,0 +1,18 @@
+
+public class gugudand {
+	public static void main(String[] args) {
+		
+		int i = 1;
+		while(i < 10) {
+			System.out.println(6 * i); //while 문
+			i++;
+		}
+		
+		
+		
+		for(int j = 1; j < 10; j++) {  //for문
+			
+			System.out.println(7 * j);
+		}
+	}
+}
